651f8ef433670d790717492ce237bcfc1a44c944
qwarl <quannngoc98@gmail.com>
2024-09-06 10:59:48 +0700

    plugin: twilight

---
+++ lua/plugins/twilight.lua
@@ -0,0 +1,25 @@
+local toggle_twilight = false
+return {
+  "folke/twilight.nvim",
+  opts = {
+    -- your configuration comes here
+    -- or leave it empty to use the default settings
+    -- refer to the configuration section below
+  },
+  keys = {
+    {
+      "<leader>t",
+      -- "<cmd>Twilight<cr>",
+      mode = "n",
+      desc = "twilight",
+      noremap = true,
+    },
+    {
+      "<leader>tt",
+      "<cmd>Twilight<cr>",
+      mode = "n",
+      desc = "Toggle Twilight",
+      noremap = true,
+    },
+  },
+}
