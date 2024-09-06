2de4627bb65de54f34ed346c9383c953859e9402
qwarl <quannngoc98@gmail.com>
2024-09-06 11:00:12 +0700

    plugin: zen-mode

---
+++ lua/plugins/zen-mode.lua
@@ -0,0 +1,13 @@
+return {
+  "folke/zen-mode.nvim",
+  otps = {},
+  keys = {
+    {
+      "<leader>zm",
+      "<cmd>ZenMode<cr>",
+      mode = "n",
+      desc = "Zen Mode",
+      noremap = true,
+    },
+  },
+}
