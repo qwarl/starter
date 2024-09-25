4ae6c43ebe632859fb6a8f461626ed129c40054d
qwarl <quannngoc98@gmail.com>
2024-09-25 11:19:53 +0700

    plugin: markview.lua

---
+++ lua/plugins/markview.lua
@@ -0,0 +1,29 @@
+return {
+  "OXY2DEV/markview.nvim",
+  lazy = false, -- Recommended
+  -- ft = "markdown" -- If you decide to lazy-load anyway
+
+  dependencies = {
+    -- You will not need this if you installed the
+    -- parsers manually
+    -- Or if the parsers are in your $RUNTIMEPATH
+    -- "nvim-treesitter/nvim-treesitter",
+
+    -- "nvim-tree/nvim-web-devicons"
+  },
+  keys = {
+    {
+      "<leader>m",
+      mode = "n",
+      desc = "Markdown",
+      noremap = true,
+    },
+    {
+      "<leader>mt",
+      "<cmd>Markview<cr>",
+      mode = "n",
+      desc = "Markdown Toggle",
+      noremap = true,
+    },
+  },
+}
