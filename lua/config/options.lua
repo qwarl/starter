11e3d64d932dee5d48963f9d425df9b9fdd7b6d7
qwarl <quannngoc98@gmail.com>
2024-10-05 00:19:15 +0700

    update options: display number and relative number at the same time

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,8 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- if wanna have line numbers and relative numbers together, use the following
+-- vim.opt.number = true
+-- vim.opt.relativenumber = true
+-- vim.o.statuscolumn = "%s %l %r "
